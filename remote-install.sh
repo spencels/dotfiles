ed7c406e41110565a620a3505ae8e24576a85247
Spencer Stephens <spencer.leo.stephens@gmail.com>
2021-03-10 17:34:07 -0800

    Make remote-install script work.

--- remote-install.sh
+++ remote-install.sh
@@ -2,13 +2,13 @@
 # "Remote install" script meant to be piped directly to bash.
 
 function main {
-  local working_dir=`mkdtemp -d`
+  local working_dir=`mktemp -d`
 
   git clone https://github.com/spencels/dotfiles.git "$working_dir"
 
   echo "Installing files..."
   "$working_dir/install.sh"
   echo "Cleaning up."
-  rm "$working_dir"
+  rm -rf "$working_dir"
 }
 main
